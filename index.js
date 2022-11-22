ec897375a7389960ae757a3325bc360c8192f494
Fabricio Oliveira <83975625+FabricioOliveira1@users.noreply.github.com>
2022-11-21 23:26:13 -0300

    upload js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 
 const url = "http://api.github.com/users/FabricioOliveira1"
 
-function getImgGit () {
-  fetch(url)
+async function getImgGit () {
+  await fetch(url)
   .then(response => response.json())
   .then(response => sendImgToHtml(response.avatar_url))
   .catch(error => console.error(error))
